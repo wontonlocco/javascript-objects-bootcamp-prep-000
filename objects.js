dde3e33e2c060145e68120d861840c862822761b
Ed Wright <ew_wrightiii@live.com>
2017-12-15 15:07:24 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = new Object({
+  ['Tay-k']: 'The Race'
+})
+//nondestructively update obj playlist with artistName and songTitle
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({},
+  playlist, {'Phil Ochs': 'Here\'s to the State of Mississippi' })
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  
+  return 
+}
