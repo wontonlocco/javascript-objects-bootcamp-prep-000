4ecc03a03f4119796d845ab90c1e56e828bef983
Ed Wright <ew_wrightiii@live.com>
2017-12-15 15:35:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({},
   playlist, {'Phil Ochs': 'Here\'s to the State of Mississippi' })
 }
-
+//Here once the function is called the artistName would be replaced by a parameter indicating which artistName is deleted
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
   return playlist
